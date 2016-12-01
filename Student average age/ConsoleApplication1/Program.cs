2415a96f44ccd1e60c30d753e7c8c7257203d7a6
KARTHEEK264 <KG263@NJIT.EDU>
2016-12-01 08:32:24 -0600

    StudentAVG age

---
+++ Student average age/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSample
+{
+    struct Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> student = new List<Student>()
+        {
+            new Student() { ID = 1, Name = "John", Age =21},
+            new Student() { ID = 2, Name = "Joe", Age =24},
+            new Student() { ID = 3, Name = "Jason", Age =25},
+            new Student() { ID = 4, Name = "Joy", Age =22},
+            new Student() { ID = 5, Name = "Joseph", Age =23},
+        };
+
+           // Student match = student.Find(s => s.ID == 3);
+            double averageAge = student.Average(s => s.Age);
+            Console.WriteLine($"Average age is {averageAge}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
